6447e42d7c4888ab906a194fde4779b5870ec283
Michal Muszynski <hi@geodev.me>
2025-06-22 20:28:46 +0200

    minor fix

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,26 +1,27 @@
 import Image, { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('/src/assets');
-  eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy("/src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
   eleventyConfig.addPassthroughCopy({ "src/assets/js": "assets/js" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/imgs": "assets/imgs" });
 
   eleventyConfig.addWatchTarget("src/assets/js");
 
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 
-  eleventyConfig.addNunjucksAsyncShortcode("svgIcon", async filename => {
+  eleventyConfig.addNunjucksAsyncShortcode("svgIcon", async (filename) => {
     const metadata = await Image(`./src/assets/icons/${filename}`, {
       formats: ["svg"],
       dryRun: true,
-    })
-    return metadata.svg[0].buffer.toString()
-  })
+    });
+    return metadata.svg[0].buffer.toString();
+  });
 
   return {
     dir: {
-      input: 'src',
-      output: 'public'
+      input: "src",
+      output: "public",
     },
   };
-};
+}
